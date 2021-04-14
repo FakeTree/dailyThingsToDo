ecc07bb103d8805f45fdca40b1f0f51e480613fa
William Halliday <williamhallidayaus@gmail.com>
2021-04-14 21:03:41 +1000

    Create Local Functionality is Working
    Can now create new head tasks and make sub tasks below them. Next is making it save to json then on open it opens with data previously made

--- build/js/observer.js
+++ build/js/observer.js
@@ -1,11 +1,33 @@
 document.getElementById("createNewElement").onclick = ()=>{
-    var template = document.getElementById("unsignedContainer");
-    var content = template.content.cloneNode(true);
-    document.getElementById("signedItem").appendChild(content);
+    const template = document.getElementById("unsignedContainer");
+    const content = template.content.cloneNode(true);
+    const location = document.getElementById("signedItem");
+    location.append(content);
+    
+    // Set's the taskNum to the ID of the task
+    let count = location.childElementCount-1;
+    let childElement = location.children[count];
+    childElement.setAttribute("taskNum", count);
+    childElement.children[1].id = "subTaskContainerValue" + count;
+    childElement.children[0].id = "taskContainerValue" + count;
+    childElement.children[0].onclick = ()=>{openSubTasks(count)};
+    childElement.children[0].children[2].onclick = ()=>{createSubTask(count)};
 }
 
-document.getElementById("createNewElement").onclick = ()=>{
-    var template = document.getElementById("unsignedContainer");
-    var content = template.content.cloneNode(true);
-    document.getElementById("signedItem").appendChild(content);
+function openSubTasks(taskValue){
+    //console.log(`document.getElementById(subTaskContainerValue${taskValue});`);
+    let location = document.getElementById("subTaskContainerValue"+taskValue);
+    location.classList.add("openSubTasks");
+}
+
+function createSubTask(taskValue){
+    const template = document.getElementById("unsignedSubItem")
+    const content = template.content.cloneNode(true)
+    let location = document.getElementById("subTaskContainerValue"+taskValue);
+
+    location.append(content)
+
+    // Set's the subTaskNum to the ID of the sub task
+    let count = location.childElementCount-1;
+    location.children[count].setAttribute("subTaskNum", count);
 }
